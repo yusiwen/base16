f53a722e1dbea6ff6863342077a0e0b1df8058df
Wynn Netherland <wynn@github.com>
2013-04-08 13:57:35 -0500

    Allow URL hash-based theme switching

--- js/switch.js
+++ js/switch.js
@@ -1,20 +1,32 @@
 $(function() {
-  $( '#scheme-select a' ).click( function( evt ) {
-    evt.preventDefault();
+  // $( '#scheme-select a' ).click( function( evt ) {
+  //   evt.preventDefault();
 
-    var $this = $( this ),
-      file = $this.attr( 'href' ).substring( 1 ),
-      $link = $( '#scheme' );
+  //   var $this = $( this ),
+  //     file = $this.attr( 'href' ).substring( 1 ),
+  //     $link = $( '#scheme' );
 
-    $.ajax('css/base16-' + file + '.css').complete(function( data ) {
-      $link.remove();
-      $( '.scheme' ).text( 'Preview: ' + file.charAt(0).toUpperCase() + file.substring( 1 ) );
-      swapOut( data.responseText );
-    });
+  //   $.ajax('css/base16-' + file + '.css').complete(function( data ) {
+  //     $link.remove();
+  //     $( '.scheme' ).text( 'Preview: ' + file.charAt(0).toUpperCase() + file.substring( 1 ) );
+  //     swapOut( data.responseText );
+  //   });
+  // });
+
+  // function swapOut( style ) {
+  //   var $newScheme = $( '<style id="scheme" type="text/css">' + style + '</style>' );
+  //   $( 'head' ).append( $newScheme );
+  // }
+
+  function loadTheme(theme) {
+    $('link.theme').attr('disabled', true);
+    $('link' + '.' + theme).attr('disabled', false);
+  }
+
+  $(window).hashchange( function(){
+    var theme = location.hash ? location.hash.replace('#', '') : 'default';
+    loadTheme(theme);
   });
 
-  function swapOut( style ) {
-    var $newScheme = $( '<style id="scheme" type="text/css">' + style + '</style>' );
-    $( 'head' ).append( $newScheme );
-  }
+  $(window).hashchange();
 });
