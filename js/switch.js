a9bec37c9c44bf4214d90adc5a4ec53a09c89ccb
Wynn Netherland <wynn@github.com>
2013-04-08 14:05:36 -0500

    Remove commented JS lines

--- js/switch.js
+++ js/switch.js
@@ -1,22 +1,4 @@
 $(function() {
-  // $( '#scheme-select a' ).click( function( evt ) {
-  //   evt.preventDefault();
-
-  //   var $this = $( this ),
-  //     file = $this.attr( 'href' ).substring( 1 ),
-  //     $link = $( '#scheme' );
-
-  //   $.ajax('css/base16-' + file + '.css').complete(function( data ) {
-  //     $link.remove();
-  //     $( '.scheme' ).text( 'Preview: ' + file.charAt(0).toUpperCase() + file.substring( 1 ) );
-  //     swapOut( data.responseText );
-  //   });
-  // });
-
-  // function swapOut( style ) {
-  //   var $newScheme = $( '<style id="scheme" type="text/css">' + style + '</style>' );
-  //   $( 'head' ).append( $newScheme );
-  // }
 
   function loadTheme(theme) {
     $('link.theme').attr('disabled', true);
